ec0ca5079f2dd83cea44054eddfeba0760382f1c
YcaroCarvalho <gabrielsouza252007b@gmail.com>
2024-08-19 18:35:07 -0300

    Fix 1.2
    Fixed the error "NoSuckElement" and code improvements

--- code_1.py
+++ code_1.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import telebot
 import time
-import pyautogui 
-
+import pyautogui
 
 service = Service()
 options = webdriver.ChromeOptions()
@@ -18,94 +18,99 @@ driver.get(URL)
 ultimo_cod_link = [None]
 
 while True:
-    links = driver.find_elements(By.XPATH, '//*[@id="__next"]/main/div/fuel-grid-container/fuel-grid-item[2]/main/fuel-grid-item/div/a[1]' )
+    links = driver.find_elements(By.XPATH,  '//*[@id="__next"]/main/div/fuel-grid-container/fuel-grid-item[2]/main/fuel-grid-item/div/a[1]')
     href = [link.get_attribute('href') for link in links]
     cod_link = (href[0][-14:-1])
-    
 
     if cod_link not in ultimo_cod_link:
         ultimo_link = href
-        ultimo_cod_link.append(cod_link) 
+        ultimo_cod_link.append(cod_link)
         #ENTRARA COMANDO PARA O BOT E PARA COLETAR DADOS DO SITE
-        time.sleep(4)
+        time.sleep(1)
         print(ultimo_link)
         pyautogui.click(x=871, y=381)
 
-        time.sleep(5)
-        try:
-            produto = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[4]/div[2]/span[1]').text
-        except:
-            produto = ('não encotrado')
 
-        try:
-            veiculo = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[1]/span/a').text
-        except FileNotFoundError:                                      
-            veiculo = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[1]/span/a').text
-        except :
-            veiculo = ('Não encontrado')
-                                                                                                
-                                                                                            
-        try:
-            carroceria = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[2]/span/a[1]').text
-        except FileNotFoundError: 
-            carroceria = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[2]/span/a[1]').text 
-        except :                                              
-            carroceria = ('Não encontrado')
+        def search_xpaths(xpaths):
+            for xpath in xpaths:
+                try:
+                    informacao = driver.find_element(By.XPATH, xpath).text
+                    return informacao
+                except NoSuchElementException:
+                    pass
 
-        try:
-            tipo_carga = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[5]/span[1]').text
-        except FileNotFoundError: 
-            tipo_carga = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[5]/span').text  
-        except :
-            tipo_carga = ('Não encontrado')  
 
-        try:
-            rastreamento = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[6]/span').text
-        except FileNotFoundError: 
-            rastreamento = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[6]/span').text
-        except :
-            rastreamento = ('Não encontrado')
+        #COLETA INFORMAÇÕES DO PRODUTO
+        xpaths = [
+            "/html/body/div[3]/div/div[1]/div/div[3]/div[2]/span[1]",
+            "/html/body/div[3]/div/div[1]/div/div[4]/div[2]/span[1]",
+            '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/span[1]',
+            '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/span[1]',
+            '/html/body/div[3]/div/div[1]/div/div[7]/div[2]/span[1]', ]
 
-        try:
-            agenciamento = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[7]/span').text
-        except FileNotFoundError: 
-            agenciamento = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[7]/span').text
-        except :
-            agenciamento = ('Não encontrado')
-        
-        try:
-            origem_city = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[1]/span/a[1]').text
-        except :
-            origem_city= ('Não encontrado')                                            
-        try:
-            origem_estado = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[1]/span/a[2]').text
-        except :
-            origem_estado= ('Não encontrado') 
+        produto = search_xpaths(xpaths)
 
-        origem = (origem_city, origem_estado)
+        #COLETA INFORMAÇÕES DO VEICULO
 
-        try:
-            destino_city = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[3]/span/a[1]').text
-        except :
-            destino_city = ('Não encontrado') 
+        xpaths = ['/html/body/div[3]/div/div[1]/div/div[3]/div[2]/div[1]/span/a'
+                  '/html/body/div[3]/div/div[1]/div/div[4]/div[2]/div[1]/span/a',
+                  '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[1]/span/a',
+                  '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[1]/span/a',
+                  '/html/body/div[3]/div/div[1]/div/div[7]/div[2]/div[1]/span/a'
+                  ]
+        veiculo = search_xpaths(xpaths)
 
-        try:
-            destino_estado = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[3]/span/a[2]').text
-        except :
-            destino_estado = ('Não encontrado') 
-        destino = (destino_city , destino_estado)
-       
+        # COLETA INFORMAÇÕES DA CARROCERIA
+        xpaths = ['/html/body/div[3]/div/div[1]/div/div[3]/div[2]/div[2]/span/a',
+                  '/html/body/div[3]/div/div[1]/div/div[4]/div[2]/div[2]/span/a',
+                  '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[2]/span/a',
+                  '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[2]/span/a',
+                  '/html/body/div[3]/div/div[1]/div/div[7]/div[2]/div[2]/span/a'
+                  ]
+        carroceria = search_xpaths(xpaths)
 
-        bot.send_message(CHAT_ID, text= f'Link: {ultimo_link}\n Produto: {produto}\n Veiculo: {veiculo}\n Origem: {origem} \n Destino: {destino}\n Carroceria:{carroceria}\n Tipo carga:{tipo_carga}')
+        # COLETA INFORMAÇÕES DO TIPO DA CARGA
+        xpaths = ['/html/body/div[3]/div/div[1]/div/div[3]/div[2]/div[5]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[4]/div[2]/div[5]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[5]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[5]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[7]/div[2]/div[5]/span'
+                  ]
+        tipo_carga = search_xpaths(xpaths)
+
+        # COLETA INFORMAÇÕES DO RASTREIO
+        xpaths = ['/html/body/div[3]/div/div[1]/div/div[3]/div[2]/div[6]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[4]/div[2]/div[6]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[6]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[6]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[7]/div[2]/div[6]/span'
+                  ]
+        search_xpaths(xpaths)
+
+        #INFORMAÇÕES SOBRE O AGENCIAMENTO
+        xpaths = ['/html/body/div[3]/div/div[1]/div/div[3]/div[2]/div[7]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[4]/div[2]/div[7]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[5]/div[2]/div[7]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[6]/div[2]/div[7]/span',
+                  '/html/body/div[3]/div/div[1]/div/div[7]/div[2]/div[7]/span',
+                  ]
+        search_xpaths(xpaths)
+
+        #ORIGEM E DESTINO
+        origem_city = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[1]/span/a[1]').text
+        origem_estado = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[1]/span/a[2]').text
+        origem = f'{origem_city} - {origem_estado}'
+        origem = ('').join(origem)
+
+        destino_city = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[3]/span/a[1]').text
+        destino_estado = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div/div[3]/div[3]/span/a[2]').text
+        destino = f'{destino_city} - {destino_estado}'
+        destino = ('').join(destino)
+
+        bot.send_message(CHAT_ID, text=f'Link: {ultimo_link}\n Produto: {produto}\n Veiculo: {veiculo}\n Origem: {origem} \n Destino: {destino}\n Carroceria: {carroceria}\n Tipo carga: {tipo_carga}')
 
         driver.back()
 
     else:
-        time.sleep(2)
+        time.sleep(0.5)
         driver.refresh()
-        
-
-        
-
-    
-
